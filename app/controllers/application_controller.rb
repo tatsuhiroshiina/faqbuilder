0f282dac5906302e25acecf554c73a67ca9f2ecd
ej <tatsuhiroshiina123@gmail.com>
2021-12-19 00:12:58 +0900

    fe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,13 +23,13 @@ class ApplicationController < ActionController::Base
 
   private
   # 配列内にアクセスを許可するIPアドレスを羅列する。
-  PERMIT_ADDRESSES = ['192.168.11.3', '36.2.33.230', '::1'].freeze
+  #PERMIT_ADDRESSES = ['192.168.11.3', '36.2.33.230', '::1'].freeze
 
-  def restrict_remote_ip
-  unless PERMIT_ADDRESSES.include?(request.remote_ip)
-    redirect_to("https://www.google.com")
-  end
-  end
+  #def restrict_remote_ip
+  #unless PERMIT_ADDRESSES.include?(request.remote_ip)
+  #  redirect_to("https://www.google.com")
+  #end
+  #end
   protected
 
   # 入力フォームからアカウント名情報をDBに保存するために追加
